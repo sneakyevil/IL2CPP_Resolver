702041acf33ed43ad893482692dc37aa826f57e2
sneakyevil <sneakyevildogs@gmail.com>
2021-08-24 04:35:43 +0200

    Update System_String.hpp

--- Unity/Structures/System_String.hpp
+++ Unity/Structures/System_String.hpp
@@ -9,13 +9,17 @@ namespace Unity
 
 		void Clear()
 		{
-			memset(m_wString, 0, static_cast<size_t>(m_iLength * 2));
+			if (!this) return;
+
+			memset(m_wString, 0, static_cast<size_t>(m_iLength) * 2);
 			m_iLength = 0;
 		}
 
 		std::string ToString()
 		{
-			std::string sRet(m_iLength + 1, '\0');
+			if (!this) return "";
+
+			std::string sRet(static_cast<size_t>(m_iLength) + 1, '\0');
 			WideCharToMultiByte(CP_UTF8, 0, m_wString, m_iLength, &sRet[0], m_iLength, 0, 0);
 			return sRet;
 		}
