a8dd1c06de3936647958c5850fcda33ade3a7d36
sneakyevil <sneakyevildogs@gmail.com>
2021-11-28 22:16:59 +0100

    NoGC for String

--- Functions/String.cpp
+++ Functions/String.cpp
@@ -10,5 +10,19 @@ namespace IL2CPP
 		}
 
 		Unity::System_String* New(std::string m_sString) { return New(&m_sString[0]); }
+
+		namespace NoGC
+		{
+			Unity::System_String* New(const char* m_pString)
+			{
+				Unity::System_String* m_pNew = new Unity::System_String;
+				m_pNew->m_pClass = IL2CPP::Class::Find("System.String");
+				m_pNew->m_iLength = swprintf_s(m_pNew->m_wString, (sizeof(Unity::System_String::m_wString) / 4), L"%hs", m_pString);
+
+				return m_pNew;
+			}
+
+			Unity::System_String* New(std::string m_sString) { return New(&m_sString[0]); }
+		}
 	}
 }
--- Functions/String.hpp
+++ Functions/String.hpp
@@ -7,5 +7,13 @@ namespace IL2CPP
 		Unity::System_String* New(const char* m_pString);
 
 		Unity::System_String* New(std::string m_sString);
+
+		// Make sure to delete it after you done using it.
+		namespace NoGC
+		{ 
+			Unity::System_String* New(const char* m_pString);
+
+			Unity::System_String* New(std::string m_sString);
+		}
 	}
 }
