9c515712b6c0ebdcadf7661a3af7561a708227c6
ExtremeBlackLiu <54531236+extremeblackliu@users.noreply.github.com>
2023-01-17 16:13:22 +0800

    Update il2cppDictionary.hpp

--- Unity/Structures/il2cppDictionary.hpp
+++ Unity/Structures/il2cppDictionary.hpp
@@ -2,12 +2,18 @@
 
 namespace Unity
 {
-	// Don't use! Untested
-	template<typename T>
+	template<typename TKey,typename TValue>
 	struct il2cppDictionary : il2cppObject
 	{
+		struct Entry
+		{
+			int m_iHashCode;
+			int m_iNext;
+			TKey m_tKey;
+			TValue m_tValue;
+		};
 		il2cppArray<int>* m_pBuckets;
-		il2cppArray<T>* m_pEntries;
+		il2cppArray<Entry*>* m_pEntries;
 		int m_iCount;
 		int m_iVersion;
 		int m_iFreeList;
@@ -15,5 +21,42 @@ namespace Unity
 		void* m_pComparer;
 		void* m_pKeys;
 		void* m_pValues;
+
+		Entry* GetEntry()
+		{
+			return (Entry*)m_pEntries->GetData();
+		}
+
+		TKey GetKeyByIndex(int iIndex)
+		{
+			TKey tKey = { 0 };
+			
+			Entry* pEntry = GetEntry();
+			if (pEntry)
+				tKey = pEntry[iIndex].m_tKey;
+
+			return tKey;
+		}
+
+		TValue GetValueByIndex(int iIndex)
+		{
+			TValue tValue = { 0 };
+
+			Entry* pEntry = GetEntry();
+			if (pEntry)
+				tValue = pEntry[iIndex].m_tValue;
+
+			return tValue;
+		}
+
+		TValue GetValueByKey(TKey tKey)
+		{
+			TValue tValue = { 0 };
+			for (int i = 0; i < m_iCount; i++) {
+				if (GetEntry()[i].key == key)
+					tValue =  GetEntry()[i].value;
+			}
+			return tValue;
+		}
 	};
 }
