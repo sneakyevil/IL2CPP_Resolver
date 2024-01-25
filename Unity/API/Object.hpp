bd06bfb689d0ec68ae407d56c16b17d0b42cccd2
ExtremeBlackLiu <extremeblackliu@gmail.com>
2024-01-26 03:43:29 +0800

    Update Object.hpp

--- Unity/API/Object.hpp
+++ Unity/API/Object.hpp
@@ -41,36 +41,36 @@ namespace Unity
 		}
 	
 		template<typename T>
-		static il2cppArray<T*>* FindObjectsOfType(il2cppObject* m_pSystemType)
+		static il2cppArray<T*>* FindObjectsOfType(il2cppObject* m_pSystemType, bool m_pIncludeInactive = false)
 		{
-			return reinterpret_cast<Unity::il2cppArray<T*>*(UNITY_CALLING_CONVENTION)(void*)>(m_ObjectFunctions.m_FindObjectsOfType)(m_pSystemType);
+			return reinterpret_cast<Unity::il2cppArray<T*>*(UNITY_CALLING_CONVENTION)(void*, bool)>(ObjectFunctions.m_pFindObjectsOfType)(m_pSystemType, m_pIncludeInactive);
 		}
 
 		template<typename T>
-		static il2cppArray<T*>* FindObjectsOfType(const char* m_pSystemTypeName)
+		static il2cppArray<T*>* FindObjectsOfType(const char* m_pSystemTypeName, bool m_pIncludeInactive = false)
 		{
 			il2cppClass* m_pClass = IL2CPP::Class::Find(m_pSystemTypeName);
 			if (!m_pClass) return nullptr;
 
-			return FindObjectsOfType<T>(IL2CPP::Class::GetSystemType(m_pClass));
+			return FindObjectsOfType<T>(IL2CPP::Class::GetSystemType(m_pClass), m_pIncludeInactive);
 		}
 
 		template<typename T>
-		static T* FindObjectOfType(il2cppObject* m_pSystemType)
+		static T* FindObjectOfType(il2cppObject* m_pSystemType, bool m_pIncludeInactive = false)
 		{
-			il2cppArray<T*>* m_pArray = FindObjectsOfType<T>(m_pSystemType);
+			il2cppArray<T*>* m_pArray = FindObjectsOfType<T>(m_pSystemType, m_pIncludeInactive);
 			if (!m_pArray || m_pArray->m_uMaxLength == 0U) return nullptr;
 
 			return m_pArray->m_pValues[0];
 		}
 
 		template<typename T>
-		static T* FindObjectOfType(const char* m_pSystemTypeName)
+		static T* FindObjectOfType(const char* m_pSystemTypeName, bool m_pIncludeInactive = false)
 		{
 			il2cppClass* m_pClass = IL2CPP::Class::Find(m_pSystemTypeName);
 			if (!m_pClass) return nullptr;
 
-			return FindObjectOfType<T>(IL2CPP::Class::GetSystemType(m_pClass));
+			return FindObjectOfType<T>(IL2CPP::Class::GetSystemType(m_pClass), m_pIncludeInactive);
 		}
 	}
 }
