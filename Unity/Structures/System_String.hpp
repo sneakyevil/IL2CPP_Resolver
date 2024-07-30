251051e4f2f6b2b6f3a8169d65f3739317e1de08
ExtremeBlackLiu <extremeblackliu@gmail.com>
2024-07-30 15:49:52 +0800

    incorrect string length calculate
    this is temp fix and not really proper convert.

--- Unity/Structures/System_String.hpp
+++ Unity/Structures/System_String.hpp
@@ -19,7 +19,7 @@ namespace Unity
 		{
 		    if (!this) return "";
 
-		    std::string sRet(static_cast<size_t>(m_iLength) * 3 + 1, '\0');
+		    std::string sRet(static_cast<size_t>(m_iLength + 1) * 4, '\0');
 		    WideCharToMultiByte(CP_UTF8, 0, m_wString, m_iLength, &sRet[0], static_cast<int>(sRet.size()), 0, 0);
 		    return sRet;
 		}
